259ebe60607eef91a36afb893c84cf5e522fa62e
enriiquecm <enriquecapote@icloud.com>
2024-10-25 17:39:30 +0200

    Inicio Front

---
+++ Frontend/FrontRetroKits/App.js
@@ -0,0 +1,8 @@
+import './App.css'
+import {Carrousel} from './components/carrousel.jsx'
+
+function App() {
+  <Carrousel />
+}
+
+export default App
---
+++ Frontend/FrontRetroKits/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./styles/carrousel.css"
+
+
+import Carrousel from "./components/carrousel.jsx";
+
+function App() {
+  return (
+    <>
+      <Carrousel />
+    </>
+  );
+}
+export default App
