ba45927fdd1b0c4da4d6b8f30fe6c11598cc6e30
alonsoOnsurbe <olalonso831@gmail.com>
2024-10-25 18:55:31 +0200

    primera subida de backend

---
+++ Backend/RetroKits/RetroKits/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RetroKits.Database;
+using RetroKits.Repository;
+
+namespace RetroKits.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly UserRepository _userRepository;
+
+    public UsersController(UserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<User>> GetAllAsync()
+    {
+        return await _userRepository.GetAllAsync();
+    }
+}
---
+++ Backend/RetroKits/RetroKits/Repository/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetroKits.Database;
+
+namespace RetroKits.Repository;
+
+public class UserRepository : Repository<User>
+{
+    public UserRepository(MyDbContext context) : base(context) 
+    {
+    }
+}
