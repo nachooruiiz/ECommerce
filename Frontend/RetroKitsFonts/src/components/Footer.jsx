2d3d80aae952068d2e48ca1060ce2d6c6b6f370b
unknown <mgaralc807e@g.educaand.es>
2024-10-28 19:46:29 +0100

    Footer terminado

--- Frontend/RetroKitsFonts/src/components/Footer.jsx
+++ Frontend/RetroKitsFonts/src/components/Footer.jsx
@@ -1,29 +1,42 @@
-import '../css/FooterStyles.css'
+import '../css/FooterStyles.css';
 
 export function Footer() {
     return (
         <footer>
-            <img className="img-footer" src="" alt="La imagen no se puede cargar"/>
-            <nav className='nav-footer'>
-                <div>
-                    <h3>Sobre nosotros</h3>
-                    <p>Quienes somos</p>
-                    <p>Qué hacemos</p>
+            <div className="footer-top">
+                <div className="containerImg">
+                    <img className="img-footer" src="public/Imagenes/Logo.png" alt="La imagen no se puede cargar"/>
                 </div>
-                <div>
-                    <h3>Políticas</h3>
-                    <p>Aviso de privacidad</p>
-                    <p>Política de coockies</p>
-                    <p>Área legal</p>
-                </div>
-                <div>
-                    <h3>Nuestras ligas</h3>
-                    <p>adfasdf</p>
-                </div>
-            </nav>
-            <nav className='nav-icons'>
+                
+                <nav className="nav-footer">
+                    <div>
+                        <h3>Sobre nosotros</h3>
+                        <p>Quienes somos</p>
+                        <p>Qué hacemos</p>
+                    </div>
+                    <div>
+                        <h3>Políticas</h3>
+                        <p>Aviso de privacidad</p>
+                        <p>Política de cookies</p>
+                        <p>Área legal</p>
+                    </div>
+                    <div>
+                        <h3>Nuestras ligas</h3>
+                        <p>Aviso de privacidad</p>
+                        <p>Política de coockies</p>
+                        <p>Área legal</p>
+                    </div>
+                </nav>
+            </div>
+            <nav className="nav-icons">
                 <p>Síguenos en:</p>
-                <div>Redes sociales</div>
+                <div className="social-icons">
+                    <img src="public/Imagenes/facebook.png" alt="Facebook" />
+                    <img src="public/Imagenes/twitter.png" alt="Twitter" />
+                    <img src="public/Imagenes/instagram.png" alt="Instagram" />
+                    <img src="public/Imagenes/linkedin.png" alt="LinkedIn" />
+                    <img src="public/Imagenes/youtube.png" alt="YouTube" />
+                </div>
             </nav>
         </footer>
     );
