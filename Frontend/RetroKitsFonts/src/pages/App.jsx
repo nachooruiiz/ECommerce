e6065ef8a957f9051b4d9818f5962d383fb3811b
Alonso <olalonso831@gmail.com>
2024-10-31 13:51:53 +0100

    home añadido, faltan imagenes

--- Frontend/RetroKitsFonts/src/pages/App.jsx
+++ Frontend/RetroKitsFonts/src/pages/App.jsx
@@ -1,9 +1,10 @@
+import './../css/estilosGenerales.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import {Header}  from "./../components/Header";
 import {Footer} from "./../components/Footer";
 import LoginForm  from "./../components/LoginForm";
 import RegisterForm from "./../components/RegisterForm";
-import './../css/estilosGenerales.css';
+import Home from "./../components/Home";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes className="margen-arriba">
         <Route path="/" element={<LoginForm />} />
         <Route path="/Register" element={<RegisterForm/>} />
+        <Route path="/Home" element={<Home/>}/>
       </Routes>
       <Footer />
     </Router>
