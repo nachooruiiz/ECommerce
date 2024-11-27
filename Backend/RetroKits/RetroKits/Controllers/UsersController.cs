ac8f4492fb08f7a76f7aedb370514d6500e09233
Alonso <olalonso831@gmail.com>
2024-11-27 20:48:42 +0100

    editar usuario hecho

--- Backend/RetroKits/RetroKits/Controllers/UsersController.cs
+++ Backend/RetroKits/RetroKits/Controllers/UsersController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RetroKits.Database;
+using RetroKits.Models;
 using RetroKits.Repository;
+using System.Security.Claims;
 
 namespace RetroKits.Controllers;
 
@@ -10,10 +13,12 @@ namespace RetroKits.Controllers;
 public class UsersController : ControllerBase
 {
     private readonly UserRepository _userRepository;
+    private readonly MyDbContext _dbContext;
 
-    public UsersController(UserRepository userRepository)
+    public UsersController(UserRepository userRepository, MyDbContext dbContext)
     {
         _userRepository = userRepository;
+        _dbContext = dbContext;
     }
 
     [HttpGet]
@@ -22,4 +27,44 @@ public class UsersController : ControllerBase
         return await _userRepository.GetAllAsync();
     }
 
+    [HttpPut("UpdateItem/{userId}")]
+    public IActionResult UpdateUser([FromRoute] int userId, [FromBody] UserDto changes)
+    {
+        var existingUser = _dbContext.Users.SingleOrDefault(o => o.Id == userId);
+
+        if (existingUser == null)
+        {
+            return NotFound("No se encontró el usuario.");
+        }
+
+        if (!string.IsNullOrEmpty(changes.Name)) 
+        {
+            existingUser.Name = changes.Name;
+        }
+        if (!string.IsNullOrEmpty(changes.Addres))
+        {
+            //existingUser = changes.Addres;
+        }
+        if (!string.IsNullOrEmpty(changes.Email))
+        {
+            existingUser.Email = changes.Email;
+        }
+
+        _dbContext.SaveChanges();
+
+        return Ok("Usuario actualizado correctamente.");
+    }
+
+    [HttpDelete("DeleteUser")]
+    public IActionResult DeleteUser()
+    {
+        var userId = int.Parse(User.FindFirstValue("id"));
+
+        var user = _dbContext.Users.FirstOrDefault(o => o.Id == userId);
+
+        _dbContext.Users.Remove(user);        
+        _dbContext.SaveChanges();
+
+        return Ok("Usuario eliminado exitosamente.");
+    }
 }
---
+++ Backend/RetroKits/RetroKits/Models/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RetroKits.Models;
+
+public class UserDto
+{
+    public string Name {  get; set; }
+    public string Email { get; set; }
+    public string Addres { get; set; }
+}
